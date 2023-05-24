29fbff94982df4380d7dc219ed072d4747ef21e7
fdk2zhang <44690561+fdk2zhang@users.noreply.github.com>
2023-05-24 23:54:21 +0800

    Update Cachet.java
    这是备注描述

--- sixmanwork/src/main/java/com/been/Cachet.java
+++ sixmanwork/src/main/java/com/been/Cachet.java
@@ -60,7 +60,7 @@ public class Cachet {
     public String getRemark() {
         return remark;
     }
-	//备注
+	//备注fdk
     public void setRemark(String remark) {
         this.remark = remark == null ? null : remark.trim();
     }
